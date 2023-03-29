a817d1d6b1ac0558732eee8cfdacfc8887970c83
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-28 22:05:04 -0400

    l03 demo

---
+++ lesson03/DotNetGrill/DotNetGrill/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetGrill.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
