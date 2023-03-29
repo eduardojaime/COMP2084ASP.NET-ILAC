5aca8728de262b66c3634d620fc14ba278c519e3
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-29 19:46:59 -0400

    l04 connecting to the db and creating migrations

--- lesson04/DotNetGrill/DotNetGrill/Models/OrderItem.cs
+++ lesson04/DotNetGrill/DotNetGrill/Models/OrderItem.cs
@@ -10,6 +10,7 @@
         public int ProductId { get; set; }
         public Product Product { get; set; }
         public int Quantity { get; set; }
+        // Calculated price > (product price * quantity)
         public decimal Price { get; set; }
     }
 }
