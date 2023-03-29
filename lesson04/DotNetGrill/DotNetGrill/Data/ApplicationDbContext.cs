b076256baf3a4f764a294a9f195892897ee5828b
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-29 19:26:20 -0400

    l04 part 2 data model

--- lesson04/DotNetGrill/DotNetGrill/Data/ApplicationDbContext.cs
+++ lesson04/DotNetGrill/DotNetGrill/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DotNetGrill.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotNetGrill.Data
@@ -9,5 +10,12 @@ namespace DotNetGrill.Data
             : base(options)
         {
         }
+
+        // These DbSets become Tables in the database
+        DbSet<Category> Categories { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<Cart> Carts { get; set; }
+        DbSet<OrderItem> OrderItems { get; set; }
+        DbSet<Order> Orders { get; set; }
     }
 }
