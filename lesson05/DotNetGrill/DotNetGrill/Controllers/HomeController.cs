251444dfc49c862f6d797ac584495f0354552149
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-29 20:34:43 -0400

    l05 part 1 contact us

--- lesson05/DotNetGrill/DotNetGrill/Controllers/HomeController.cs
+++ lesson05/DotNetGrill/DotNetGrill/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace DotNetGrill.Controllers
             return View();
         }
 
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
