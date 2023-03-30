e4544b36d4fb7a3483f33e3ecba097beb6956f92
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-29 20:17:18 -0400

    l05 project template

---
+++ lesson05/DotNetGrill/DotNetGrill/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DotNetGrill.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetGrill.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // These DbSets become Tables in the database
+        DbSet<Category> Categories { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<Cart> Carts { get; set; }
+        DbSet<OrderItem> OrderItems { get; set; }
+        DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ lesson05/DotNetGrill/DotNetGrill/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetGrill.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public decimal Total { get; set; }
+        public DateTime DateCreated {get;set;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
