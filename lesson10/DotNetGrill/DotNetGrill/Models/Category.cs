ccafbc4b5fdfcac07092ebd831a01d080e3c260c
Eduardo Jaime <eduardojaime86@gmail.com>
2023-03-31 07:26:37 -0400

    l10 project template

---
+++ lesson10/DotNetGrill/DotNetGrill/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using MessagePack;
+using Microsoft.Build.Framework;
+
+namespace DotNetGrill.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        // Navigation 1 to M > 1 Category is linked to Many products
+        public List<Product>? Products { get; set; }
+    }
+}
