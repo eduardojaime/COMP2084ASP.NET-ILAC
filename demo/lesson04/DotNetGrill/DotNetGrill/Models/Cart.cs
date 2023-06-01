71bcda00c2ce68cc236186216ca5b68a6c9b7d62
Eduardo Jaime <eduardojaime86@gmail.com>
2023-06-01 19:49:02 -0400

    l04 part 2 models

---
+++ demo/lesson04/DotNetGrill/DotNetGrill/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNetGrill.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public string CustomerId {  get; set; } // customer email address
+        // EF needs the following fields to link this to a Product
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        // The following are fields that will be added to the table
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateCreate { get; set; }
+        
+    }
+}
