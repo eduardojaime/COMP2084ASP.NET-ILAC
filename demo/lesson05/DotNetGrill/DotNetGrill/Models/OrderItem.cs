ca32d5cb78f21aab4197c9f7319fe345c289f750
Eduardo Jaime <eduardojaime86@gmail.com>
2023-06-08 18:37:40 -0400

    l05 project template

---
+++ demo/lesson05/DotNetGrill/DotNetGrill/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNetGrill.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        // EF needs these two fileds to link item to order
+        // M to 1
+        public int OrderId { get; set; }
+        public Order? Order { get; set; }
+        // EF needs these two fields to link item to product
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+        // db fields
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
