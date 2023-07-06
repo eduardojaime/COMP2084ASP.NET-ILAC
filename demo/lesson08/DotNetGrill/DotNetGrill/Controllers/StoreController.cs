be7fcdf646e8b3f7d777afb72ec0447dd2bad132
Eduardo Jaime <eduardojaime86@gmail.com>
2023-07-06 19:47:25 -0400

    l08 part 2 browse category page

--- demo/lesson08/DotNetGrill/DotNetGrill/Controllers/StoreController.cs
+++ demo/lesson08/DotNetGrill/DotNetGrill/Controllers/StoreController.cs
@@ -26,5 +26,23 @@ namespace DotNetGrill.Controllers
                           View(await _context.Categories.ToListAsync()) :
                           Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
         }
+
+        // GET: Store/Browse/1
+        // Gets a categoryId value and shows a filtered list of products
+        public async Task<IActionResult> Browser(int id) { 
+            // retrieve a list of products by category id
+            // use dbcontext
+            // linq query
+            var products = await _context.Products
+                .Where(p => p.CategoryId == id) // Similar to SQL WHERE P.CategoryId = @id
+                .OrderBy(p => p.Name) // lambda expression, p means 'a record in the table' or 'a product'
+                .ToListAsync();
+            // send data to the view in two ways
+            // option 1 - dynamic viewbag object
+            // find the first category that matches the id and extract the name
+            ViewBag.categoryName = _context.Categories.Find(id)?.Name; 
+            // option 2 - as a model for the view
+            return View(products);
+        }
     }
 }
