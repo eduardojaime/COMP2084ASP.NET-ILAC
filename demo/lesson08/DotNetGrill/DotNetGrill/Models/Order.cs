5fe0d6454126cb98d97a458e0d0ed7c23f6aecd8
Eduardo Jaime <eduardojaime86@gmail.com>
2023-07-06 17:34:40 -0400

    l08 project template

---
+++ demo/lesson08/DotNetGrill/DotNetGrill/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetGrill.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; } // email address
+        public decimal Total { get; set; }
+        public DateTime DateCreated { get; set; }
+        [Display(Name = "First Name")] // label shown in UI
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")] // label shown in UI
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        [Display(Name = "Postal Code")] // label shown in UI
+        public string PostalCode { get; set; }
+        // EF needs this field to link Order to OrderItems
+        // 1 to M
+        public List<OrderItem>? OrderItems { get; set; }
+    }
+}
