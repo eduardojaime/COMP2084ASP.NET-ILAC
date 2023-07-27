850406334981e32a6132e289ca1cf8ae47d2978b
Eduardo Jaime <eduardojaime86@gmail.com>
2023-07-27 18:34:23 -0400

    l11 project template

---
+++ demo/lesson11/DotNetGrill/DotNetGrill/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using DotNetGrill.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DotNetGrill.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(); // returns a view inside /Views/Home with the same name as the method
+        }
+
+        public IActionResult Privacy()
+        {
+            return View(); // returns a view inside /Views/Home with the same name as the method
+        }
+
+        // 1) add action methods in order to handle another page
+        // 2) add a corresponding view file in the folder for this controller
+        // /ContactUs
+        // action results can be JSON, XML, PLAIN TEXT, BINARY FILES OR... HTML (VIEWS)
+        public IActionResult ContactUs()
+        {
+            // returns a rendered view (HTML)
+            return View(); // returns /Views/Home/ContactUs.cshtml
+        }
+
+        // Add another action method and view for the About page
+        public IActionResult About()
+        {
+            //ViewData["Content"] = "Hello there!";
+            return View(); // returns /Views/Home/About.cshtml
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ demo/lesson11/DotNetGrill/DotNetGrill/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DotNetGrill.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetGrill.Data
+{
+    // This class represents your database
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        // Add DbSets here for EF to map them to Tables in the SQL DB
+        // Naming convention: use plural for dbset names
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
