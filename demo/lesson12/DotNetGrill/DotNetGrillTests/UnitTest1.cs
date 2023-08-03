8ec7a7fe6b060169b8f78e027b541fbb5f6476c1
Eduardo Jaime <eduardojaime86@gmail.com>
2023-08-03 18:36:09 -0400

    l12 part 1 creating a unit test project

---
+++ demo/lesson12/DotNetGrill/DotNetGrillTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+// local usings and imports
+namespace DotNetGrillTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ demo/lesson12/DotNetGrill/DotNetGrillTests/Usings.cs
@@ -0,0 +1,3 @@
+// Add global references that apply to all test classes in this project
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+// TODO: reference namespaces in the main project
