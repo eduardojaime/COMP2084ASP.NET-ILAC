87e61fd68485fdec072261d2657f8e63265a00e3
Eduardo Jaime <eduardojaime86@gmail.com>
2024-01-17 20:51:50 -0500

    l02 part 1 project templates

---
+++ demos/lesson02b/MyWebAppSolutionVSCode/MyWebAppUIVSCode/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyWebAppUIVSCode.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
