8249d9012be3838c36ebaa60f9886c86409f2662
Eduardo Jaime <eduardojaime86@gmail.com>
2024-01-30 22:00:44 -0500

    l04 part 2 models

--- demos/lesson04a/DotNetGrill/DotNetGrill/Data/ApplicationDbContext.cs
+++ demos/lesson04a/DotNetGrill/DotNetGrill/Data/ApplicationDbContext.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DotNetGrill.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotNetGrill.Data
 {
+    // this class represents your Database
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        // each dbset declaration tells EF to create a table based on that class
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product>  Products { get; set; }
     }
 }
