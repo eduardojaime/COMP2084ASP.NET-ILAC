ea39fd19b8965202a0de3da64bc3cc5345b4ca70
Eduardo Jaime <eduardojaime86@gmail.com>
2024-01-31 19:40:19 -0500

    l03 project template

---
+++ demos/lesson03b/DotNetGrill/DotNetGrillWebUI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using DotNetGrillWebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DotNetGrillWebUI.Controllers
+{
+    // Controllers represent sections in your app
+    // e.g. https://localhost:1234/HOME
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        // Each action result method handles an HTTP request to a specific subsection
+        // GET /Home/Index or / or /Home because it's the default method
+        public IActionResult Index()
+        {
+            return View(); // renders /Views/Home/Index.cshtml
+        }
+        // GET /Home/Privacy
+        public IActionResult Privacy()
+        {
+            return View(); // renders /Views/Home/Privacy.cshtml
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
