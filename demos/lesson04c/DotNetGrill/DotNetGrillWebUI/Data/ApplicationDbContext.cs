d41272f249a65fbca50d103dfc402d64789bbb5b
Eduardo Jaime <eduardojaime86@gmail.com>
2024-02-01 22:00:10 -0500

    l04 part 2 model classes

--- demos/lesson04c/DotNetGrill/DotNetGrillWebUI/Data/ApplicationDbContext.cs
+++ demos/lesson04c/DotNetGrill/DotNetGrillWebUI/Data/ApplicationDbContext.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DotNetGrillWebUI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotNetGrillWebUI.Data
 {
+    // this class represents your database
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        // dbsets will be converted into tables
+        // dbset names are plural
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
