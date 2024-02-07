c8739d1adbcf62c588d4bf53d32b89e20b22c2d8
Eduardo Jaime <eduardojaime86@gmail.com>
2024-02-06 20:30:47 -0500

    l04 part 3 configuring your app to connect to a database

--- demos/lesson04a/DotNetGrill/DotNetGrill/Data/Migrations/20240207012124_InitialModel.Designer.cs
+++ demos/lesson04a/DotNetGrill/DotNetGrill/Data/Migrations/20240207012124_InitialModel.Designer.cs
@@ -4,6 +4,7 @@ using DotNetGrill.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetGrill.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240207012124_InitialModel")]
+    partial class InitialModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
