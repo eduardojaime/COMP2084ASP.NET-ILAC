6e8c3e5341129e9d0429425db1a15d85fce77df2
Eduardo Jaime <eduardojaime86@gmail.com>
2024-02-07 20:50:38 -0500

    l05 project template and instructions

---
+++ demos/lesson05b/DotNetGrill/DotNetGrillWebUI/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using DotNetGrillWebUI.Models;
+
+namespace DotNetGrill.Models
+{
+    public class Cart
+    {
+        // Think of cart as a temporary list of selected products the user intends to pay for
+        // Once user pays and order is created, these records are cleared
+        public int CartId { get; set; }
+        // it will be associated to the Customer by GUID or email address
+        public string CustomerId { get; set; }
+        // Links 1 to 1 to Products > 1 product is 1 cart item
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ demos/lesson05b/DotNetGrill/DotNetGrillWebUI/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetGrill.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public decimal Total { get; set; }
+        public DateTime DateCreated {get;set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+        // 1 to M relationship to OrderItems
+        public List<OrderItem>? OrderItems { get; set; }
+    }
+}
