b897cc4e7f1270508cba40c0c56c98fa42249e21
Eduardo Jaime <eduardojaime86@gmail.com>
2024-02-08 20:55:50 -0500

    l05 project template and instructions

---
+++ demos/lesson05c/DotNetGrill/DotNetGrillWebUI/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetGrillWebUI.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public decimal Total { get; set; }
+        public DateTime DateCreated {get;set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+        // 1 to M relationship from Order to OrderItems
+        // 1 part contains a list 
+        public List<OrderItem>? OrderItems { get; set; }
+    }
+}
