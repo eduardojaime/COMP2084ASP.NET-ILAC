64547159ccf6668000c484fa4bca88e351d0c3ee
Eduardo Jaime <eduardojaime86@gmail.com>
2024-03-05 21:05:26 -0500

    l08 part 2 adding browse section

--- demos/lesson08a/DotNetGrill/DotNetGrill/Controllers/StoreController.cs
+++ demos/lesson08a/DotNetGrill/DotNetGrill/Controllers/StoreController.cs
@@ -24,5 +24,19 @@ namespace DotNetGrill.Controllers
         {
             return View(await _context.Categories.ToListAsync());
         }
+
+        // GET Store/Browse/ID
+        public IActionResult Browse(int id) {
+            // Filter products by id and return
+            var products = _context.Products
+                .Where(p => p.CategoryId == id) // filter by category id
+                .OrderBy(p => p.Name) // sort by name
+                .ToList(); // converts result to a list of products
+            // two ways of sending data back to the view
+            // 1) viewbag object
+            ViewBag.CategoryName = _context.Categories.Find(id).Name; // finds a category by id and gets its name
+            // 2) as a view model
+            return View(products);
+        }
     }
 }
