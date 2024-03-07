c8e3dd7c168bf0ca4e39500941a0cf93b6eb1dc8
Eduardo Jaime <eduardojaime86@gmail.com>
2024-03-06 21:20:57 -0500

    l08 part 3 add to cart

--- demos/lesson08b/DotNetGrill/DotNetGrillWebUI/Controllers/StoreController.cs
+++ demos/lesson08b/DotNetGrill/DotNetGrillWebUI/Controllers/StoreController.cs
@@ -46,5 +46,70 @@ namespace DotNetGrillWebUI.Controllers
             // 2) as a view model
             return View(products);
         }
+
+        // POST: Store/AddToCart
+        // values expected: ProductId, Quantity
+        [HttpPost]
+        public IActionResult AddToCart([FromForm] int ProductId, [FromForm] int Quantity) {
+            // get or generate a customer id
+            var customerId = GetCustomerId();
+            // query the db to get current product price
+            var price = _context.Products.Find(ProductId).Price;
+            // create and save a new cart object
+            var cart = new Cart
+            {
+                CustomerId = customerId,
+                ProductId = ProductId,
+                Quantity = Quantity,
+                DateCreated = DateTime.UtcNow, // always use UTC time to store datetimes in your dbs
+                Price = price
+            };
+            _context.Carts.Add(cart);
+            _context.SaveChanges();
+            // redirect to a "Cart" view
+            return Redirect("Cart"); // name of action method to trigger next
+        }
+
+        // GET: Store/Cart
+        public IActionResult Cart() { 
+            // retrieve customer id
+            var customerId = GetCustomerId();
+            // retrieve carts associated to them
+            var carts = _context.Carts
+                .Where(c => c.CustomerId == customerId)
+                .OrderByDescending(c => c.DateCreated)
+                .ToList();
+            // return a view with the list of carts
+            return View(carts);
+        }
+
+        private string GetCustomerId()
+        {
+            // set variable to store customerId
+            var customerId = string.Empty;
+            // check the session object for a value
+            if (String.IsNullOrEmpty(HttpContext.Session.GetString("CustomerId")))
+            {
+                // if there's no value then use either email address (authenticated users)
+                if (User.Identity.IsAuthenticated)
+                {
+                    customerId = User.Identity.Name;
+                }
+                else
+                {
+                    // or a random GUID (anonymous users)
+                    customerId = Guid.NewGuid().ToString();
+                }
+                // save the value in the session object
+                HttpContext.Session.SetString("CustomerId", customerId);
+            }
+            else
+            {
+                // return the value
+                customerId = HttpContext.Session.GetString("CustomerId");
+            }
+            // return the value
+            return customerId;
+        }
     }
 }
--- demos/lesson08b/DotNetGrill/DotNetGrillWebUI/Program.cs
+++ demos/lesson08b/DotNetGrill/DotNetGrillWebUI/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +29,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
