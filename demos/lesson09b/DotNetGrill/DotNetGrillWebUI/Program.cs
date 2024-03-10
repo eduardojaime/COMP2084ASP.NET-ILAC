278695fea0da35bbf47c13bc47a4196e81755d34
Eduardo Jaime <eduardojaime86@gmail.com>
2024-03-10 10:46:20 -0400

    l09 part 1 authorization for admins and customers

--- demos/lesson09b/DotNetGrill/DotNetGrillWebUI/Program.cs
+++ demos/lesson09b/DotNetGrill/DotNetGrillWebUI/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
