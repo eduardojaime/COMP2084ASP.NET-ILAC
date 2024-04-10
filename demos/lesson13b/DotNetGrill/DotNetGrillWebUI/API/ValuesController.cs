b154926ad7c75fd9f996a2cb65c17630d821c3a6
Eduardo Jaime <eduardojaime86@gmail.com>
2024-04-10 19:43:36 -0400

    l13 part 1 basic api controller

---
+++ demos/lesson13b/DotNetGrill/DotNetGrillWebUI/API/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotNetGrillWebUI.API
+{
+    // Handles request to localhost:12345/api/Values
+    [Route("api/[controller]")]
+    // Indicates that the class is an API controller
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // If you name the method Get
+        // it will be called when a GET request is made to the API
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
