9e0b8f9c0a5853caf843b0949009e48eaffd891f
Eduardo Jaime <eduardojaime86@gmail.com>
2024-05-28 21:33:33 -0400

    l04 models and migrations

--- demos/lesson04/DotNetGrill/DotNetGrillWebUI/Data/Migrations/20240529005925_Initial.Designer.cs
+++ demos/lesson04/DotNetGrill/DotNetGrillWebUI/Data/Migrations/20240529005925_Initial.Designer.cs
@@ -4,6 +4,7 @@ using DotNetGrillWebUI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetGrillWebUI.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240529005925_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
