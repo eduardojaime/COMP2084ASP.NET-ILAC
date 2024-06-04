12e5a31e023d9860aa891490789ea778f4e2ba51
Eduardo Jaime <eduardojaime86@gmail.com>
2024-06-04 19:49:52 -0400

    l05 part 2 about us page

--- demos/lesson05/DotNetGrill/DotNetGrillWebUI/Controllers/HomeController.cs
+++ demos/lesson05/DotNetGrill/DotNetGrillWebUI/Controllers/HomeController.cs
@@ -23,6 +23,17 @@ namespace DotNetGrillWebUI.Controllers
             return View();
         }
 
+        // Add another action method for AboutUs page
+        // IActionResult represents a result of an action method which is an HTTP Response
+        // it can contain content in different formats: JSON, HTML, XML, etc.
+        public IActionResult AboutUs()
+        {
+            // returns a ViewResult object that contains the HTML content of a view
+            // if no view name is provided, it will look for a view with the same name as the action method
+            // e.g. AboutUs.cshtml
+            return View(); 
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
