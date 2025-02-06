52bcd72fd99f9d4a2c9e4dbd87192c9d72d1a5aa
Eduardo Jaime <eduardojaime86@gmail.com>
2025-02-05 21:13:23 -0500

    l04 data model and migrations

--- demos/lesson04/DotNetGrill/DotNetGrillWebUI/Data/ApplicationDbContext.cs
+++ demos/lesson04/DotNetGrill/DotNetGrillWebUI/Data/ApplicationDbContext.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using DotNetGrillWebUI.Models;
 
+// It's already here since we selected Authentication = Individual User Accounts
 namespace DotNetGrillWebUI.Data
 {
+    // Represents my database
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        // Add dbSets, these will be mapped to tables in my database
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
---
+++ demos/lesson04/DotNetGrill/DotNetGrillWebUI/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetGrillWebUI.Models
+{
+    // Class names are in singular form
+    // This class represents a product entity in the DB (record in a table)
+    public class Product
+    {
+        // Public fields represent columns in the table
+        // Primary key, it will automatically be an identity column because it contains ID
+        public int ProductId { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Price { get; set; }
+        public int Rating { get; set; }
+        public string? Photo { get; set; } // ? means it can be null
+        // Navigation property
+        // 1 to M relationship between Product and Category
+        // A product can belong to only one category
+        // Add a categoryId and a category property
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
