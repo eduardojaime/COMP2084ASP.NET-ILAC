937e7bdb8bce657c13e936a12200d25db334d136
Eduardo Jaime <eduardojaime86@gmail.com>
2025-03-12 20:26:20 -0400

    l08 project template

---
+++ demos/lesson08/DotNetGrill/DotNetGrillWebUI/Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DotNetGrillWebUI.Data;
+using DotNetGrillWebUI.Models;
+
+namespace DotNetGrillWebUI.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StoreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Store
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+    }
+}
