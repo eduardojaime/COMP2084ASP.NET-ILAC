9992f0303c04d5b7abfb72ea3e6e8ae15ff67cb7
Eduardo Jaime <eduardojaime86@gmail.com>
2025-04-02 19:02:24 -0400

    l11 project template

---
+++ demos/lesson11/DotNetGrill/DotNetGrillWebUI/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetGrillWebUI.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        // Navigation property
+        // 1 to M relationship between Product and Category
+        // A category can have many products
+        // ? means it can be null (when first created)
+        public List<Product>? Products { get; set; }
+    }
+}
